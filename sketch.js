f34d49f9fe926dc2cae562de4b7812b11f555ca4
ctncodes <35818754+ctncodes@users.noreply.github.com>
2019-11-24 00:22:11 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -118,7 +118,7 @@ const getRandomGrid = () => {
   let grid = [[1, 2, 3, 4, 5, 6, 7, 8], [9, 10, 11, 12, 13, 14, 15, 16] [17, 18, 19, 20, 21, 22, 23, 24], [25, 26, 27, 28, 29, 30, 31, 32] [33, 34, 35, 36, 37, 38, 39, 40], [41, 42, 43, 44, 45, 46, 47, 48] [49, 50, 51, 52, 53, 54, 55, 56], [57, 58, 59, 60, 61, 62, 63, 0]];
 
   // Shuffle
-  let blankBox = new Box(3, 3);
+  let blankBox = new Box(7, 7);
   for (let i = 0; i < 1000; i++) {
     const randomNextdoorBox = blankBox.getRandomNextdoorBox();
     swapBoxes(grid, blankBox, randomNextdoorBox);
@@ -205,8 +205,8 @@ class Game {
     // Render grid
     const newGrid = document.createElement("div");
     newGrid.className = "grid";
-    for (let i = 0; i < 4; i++) {
-      for (let j = 0; j < 4; j++) {
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
         const button = document.createElement("button");
 
         if (status === "playing") {
