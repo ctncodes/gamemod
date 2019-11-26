fdb947b31dd8c6da2e0306e31bd27a21f5455f54
ctncodes <35818754+ctncodes@users.noreply.github.com>
2019-11-26 14:56:56 -0800

    2/3 of the Creator's Challenge

--- sketch.js
+++ sketch.js
@@ -1,15 +1,3 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-
-  stroke("#ffd700");
-  textSize(180);
-  fill('#662d91');
-  imageMode(CENTER);
-  frameRate(10);
-  text("Drake's Alien", 25, 325);
-  text("Assistance.", 25, 525);
-}
-
 function dis(val) {
   document.getElementById("result").value+=val
 }
@@ -141,7 +129,7 @@ const isSolved = grid => {
 };
 
 const getRandomGrid = () => {
-  let grid = [[1, 2, 3, 4, 5, 6, 7, 8], [9, 10, 11, 12, 13, 14, 15, 16] [17, 18, 19, 20, 21, 22, 23, 24], [25, 26, 27, 28, 29, 30, 31, 32] [33, 34, 35, 36, 37, 38, 39, 40], [41, 42, 43, 44, 45, 46, 47, 48] [49, 50, 51, 52, 53, 54, 55, 56], [57, 58, 59, 60, 61, 62, 63, 0]];
+  let grid = [[1, 2, 3, 4, 5, 6, 7, 8], [9, 10, 11, 12, 13, 14, 15, 16], [17, 18, 19, 20, 21, 22, 23, 24], [25, 26, 27, 28, 29, 30, 31, 32], [33, 34, 35, 36, 37, 38, 39, 40], [41, 42, 43, 44, 45, 46, 47, 48], [49, 50, 51, 52, 53, 54, 55, 56], [57, 58, 59, 60, 61, 62, 63, 0]];
 
   // Shuffle
   let blankBox = new Box(7, 7);
@@ -258,16 +246,16 @@ class Game {
     document.querySelector(".footer button").replaceWith(newButton);
 
     // Render move
-    document.getElementById("move").textContent = `Move: ${move}`;
+    document.getElementById("move").textContent = `Move ${move}`;
 
     // Render time
-    document.getElementById("time").textContent = `Time: ${time}`;
+    document.getElementById("time").textContent = `Time ${time}`;
 
     // Render message
     if (status === "won") {
       document.querySelector(".message").textContent = "Earth wins!";
-    // } else {
-    //   document.querySelector(".message").textContent = "";
+    } else {
+      document.querySelector(".message").textContent = "";
     }
   }
 }
